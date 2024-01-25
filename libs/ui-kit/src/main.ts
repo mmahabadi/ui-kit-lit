0287ef7bd8b7903b67e46dac62d8040c693e87e1
Mohsen <m.mehabadi@gmail.com>
2024-01-25 14:35:42 +0100

    add tailwind css

--- libs/ui-kit/src/main.ts
+++ libs/ui-kit/src/main.ts
@@ -1,18 +1,19 @@
 import { html, css, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { TWStyles } from './tw';
 
 @customElement('my-element')
 export class SimpleGreeting extends LitElement {
-  static styles = css`
-    p {
-      color: yellow;
-    }
-  `;
+  static styles = [css``, TWStyles];
 
   @property()
   name = 'Somebody';
 
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`<h1
+      class="text-4xl bg-blue-500 text-bold flex flex-row text-center"
+    >
+      Hello, ${this.name}!
+    </h1>`;
   }
 }
